7bd9038e8606150ad8de5f00849f1a79bb70843c
dercdevil <darwinerc92@gmail.com>
2020-08-10 20:15:31 -0400

    Commit

---
+++ build/precache-manifest.fcb735bce86136b3ae59baadcac953d0.js
@@ -0,0 +1,278 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57791f5bf99d6fc0555a557d5650c1f4",
+    "url": "https://anclick.veanx.tech/index.html"
+  },
+  {
+    "revision": "43b033d21024ebb51bc5",
+    "url": "https://anclick.veanx.tech/static/css/11.fdee07a5.chunk.css"
+  },
+  {
+    "revision": "d41239ecbf04caf1edbc",
+    "url": "https://anclick.veanx.tech/static/css/13.1e40a1d5.chunk.css"
+  },
+  {
+    "revision": "d5de630c357fd066508c",
+    "url": "https://anclick.veanx.tech/static/css/14.6d93dcf1.chunk.css"
+  },
+  {
+    "revision": "c00d0c83bb58f971581a",
+    "url": "https://anclick.veanx.tech/static/css/15.eaef173a.chunk.css"
+  },
+  {
+    "revision": "65bf086bccf96c114bf6",
+    "url": "https://anclick.veanx.tech/static/css/16.ece87755.chunk.css"
+  },
+  {
+    "revision": "03789cd7717916832d9e",
+    "url": "https://anclick.veanx.tech/static/css/18.1d4f2b34.chunk.css"
+  },
+  {
+    "revision": "31e4cb7ab875770fdf8c",
+    "url": "https://anclick.veanx.tech/static/css/19.09106ec8.chunk.css"
+  },
+  {
+    "revision": "5e36bafb325c3d0d669e",
+    "url": "https://anclick.veanx.tech/static/css/20.1e40a1d5.chunk.css"
+  },
+  {
+    "revision": "c1cf5d507cb452f5a5b1",
+    "url": "https://anclick.veanx.tech/static/css/21.1e40a1d5.chunk.css"
+  },
+  {
+    "revision": "dbe7ea799ee8dde8c6fa",
+    "url": "https://anclick.veanx.tech/static/css/22.6d93dcf1.chunk.css"
+  },
+  {
+    "revision": "190ee4ea1ddd9bd26b22",
+    "url": "https://anclick.veanx.tech/static/css/23.59c87201.chunk.css"
+  },
+  {
+    "revision": "38434664af9bf2aa220f",
+    "url": "https://anclick.veanx.tech/static/css/25.1e40a1d5.chunk.css"
+  },
+  {
+    "revision": "087ca8e86f8f9efc6edf",
+    "url": "https://anclick.veanx.tech/static/css/26.5d89b68f.chunk.css"
+  },
+  {
+    "revision": "0a781821cf1a179de49e",
+    "url": "https://anclick.veanx.tech/static/css/29.f437503a.chunk.css"
+  },
+  {
+    "revision": "c8fc3015482306e0d265",
+    "url": "https://anclick.veanx.tech/static/css/4.1e40a1d5.chunk.css"
+  },
+  {
+    "revision": "e68663e76ed5a317645a",
+    "url": "https://anclick.veanx.tech/static/css/7.07b7d53c.chunk.css"
+  },
+  {
+    "revision": "f2e2ec14761345e6adfe",
+    "url": "https://anclick.veanx.tech/static/css/8.70dbd593.chunk.css"
+  },
+  {
+    "revision": "a3bba53e2bce245f26de",
+    "url": "https://anclick.veanx.tech/static/css/9.aecb291f.chunk.css"
+  },
+  {
+    "revision": "de05167ac87d37bdf611",
+    "url": "https://anclick.veanx.tech/static/css/main.a81ac46a.chunk.css"
+  },
+  {
+    "revision": "da28fe171608ff757518",
+    "url": "https://anclick.veanx.tech/static/js/0.c62820c4.chunk.js"
+  },
+  {
+    "revision": "e1dbb0dc492abc4e6ee5",
+    "url": "https://anclick.veanx.tech/static/js/1.be2b6c3a.chunk.js"
+  },
+  {
+    "revision": "b5773f7696a7c7c20f50",
+    "url": "https://anclick.veanx.tech/static/js/10.73f0f8cd.chunk.js"
+  },
+  {
+    "revision": "43b033d21024ebb51bc5",
+    "url": "https://anclick.veanx.tech/static/js/11.67a8bd86.chunk.js"
+  },
+  {
+    "revision": "1516bcb16c5f830b74a2",
+    "url": "https://anclick.veanx.tech/static/js/12.b51e3051.chunk.js"
+  },
+  {
+    "revision": "d41239ecbf04caf1edbc",
+    "url": "https://anclick.veanx.tech/static/js/13.1e76a7ca.chunk.js"
+  },
+  {
+    "revision": "d5de630c357fd066508c",
+    "url": "https://anclick.veanx.tech/static/js/14.1eb42bcd.chunk.js"
+  },
+  {
+    "revision": "c00d0c83bb58f971581a",
+    "url": "https://anclick.veanx.tech/static/js/15.3abdc573.chunk.js"
+  },
+  {
+    "revision": "65bf086bccf96c114bf6",
+    "url": "https://anclick.veanx.tech/static/js/16.11755dfd.chunk.js"
+  },
+  {
+    "revision": "9ff5d9ceb9a2ea1b07c3",
+    "url": "https://anclick.veanx.tech/static/js/17.46599889.chunk.js"
+  },
+  {
+    "revision": "03789cd7717916832d9e",
+    "url": "https://anclick.veanx.tech/static/js/18.ff6f2084.chunk.js"
+  },
+  {
+    "revision": "31e4cb7ab875770fdf8c",
+    "url": "https://anclick.veanx.tech/static/js/19.4f5f406a.chunk.js"
+  },
+  {
+    "revision": "4afdd3fa9e7ec084217d",
+    "url": "https://anclick.veanx.tech/static/js/2.0bd19074.chunk.js"
+  },
+  {
+    "revision": "5e36bafb325c3d0d669e",
+    "url": "https://anclick.veanx.tech/static/js/20.8ab0cde0.chunk.js"
+  },
+  {
+    "revision": "c1cf5d507cb452f5a5b1",
+    "url": "https://anclick.veanx.tech/static/js/21.83e8bd33.chunk.js"
+  },
+  {
+    "revision": "dbe7ea799ee8dde8c6fa",
+    "url": "https://anclick.veanx.tech/static/js/22.7c8d9c01.chunk.js"
+  },
+  {
+    "revision": "190ee4ea1ddd9bd26b22",
+    "url": "https://anclick.veanx.tech/static/js/23.efb1f61b.chunk.js"
+  },
+  {
+    "revision": "cec0924dd8a693d831e7",
+    "url": "https://anclick.veanx.tech/static/js/24.580c7ebc.chunk.js"
+  },
+  {
+    "revision": "38434664af9bf2aa220f",
+    "url": "https://anclick.veanx.tech/static/js/25.ae2eb8cb.chunk.js"
+  },
+  {
+    "revision": "087ca8e86f8f9efc6edf",
+    "url": "https://anclick.veanx.tech/static/js/26.b83a7bec.chunk.js"
+  },
+  {
+    "revision": "df6e3a858c319a73aa6c",
+    "url": "https://anclick.veanx.tech/static/js/27.b4b7ad36.chunk.js"
+  },
+  {
+    "revision": "abb2d6b3aa38c07c0159",
+    "url": "https://anclick.veanx.tech/static/js/28.40ec7008.chunk.js"
+  },
+  {
+    "revision": "0a781821cf1a179de49e",
+    "url": "https://anclick.veanx.tech/static/js/29.be21f42b.chunk.js"
+  },
+  {
+    "revision": "a41146da6a448f28c33d",
+    "url": "https://anclick.veanx.tech/static/js/3.103428ee.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "https://anclick.veanx.tech/static/js/3.103428ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d68ef7f6d57a3bcf4c7",
+    "url": "https://anclick.veanx.tech/static/js/30.730b8979.chunk.js"
+  },
+  {
+    "revision": "9a32f26e8a6e319a6cb2",
+    "url": "https://anclick.veanx.tech/static/js/31.46a8d763.chunk.js"
+  },
+  {
+    "revision": "4bb140edbd9d4d53b3ea",
+    "url": "https://anclick.veanx.tech/static/js/32.51f05c62.chunk.js"
+  },
+  {
+    "revision": "fe2092b9534187cadf0b",
+    "url": "https://anclick.veanx.tech/static/js/33.39fe4ae5.chunk.js"
+  },
+  {
+    "revision": "c8fc3015482306e0d265",
+    "url": "https://anclick.veanx.tech/static/js/4.bcd7de0b.chunk.js"
+  },
+  {
+    "revision": "e68663e76ed5a317645a",
+    "url": "https://anclick.veanx.tech/static/js/7.c13f2043.chunk.js"
+  },
+  {
+    "revision": "aaab518a8f41881f1dce05aef7d33fb3",
+    "url": "https://anclick.veanx.tech/static/js/7.c13f2043.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2e2ec14761345e6adfe",
+    "url": "https://anclick.veanx.tech/static/js/8.721bc5f3.chunk.js"
+  },
+  {
+    "revision": "a3bba53e2bce245f26de",
+    "url": "https://anclick.veanx.tech/static/js/9.fe2e5e76.chunk.js"
+  },
+  {
+    "revision": "de05167ac87d37bdf611",
+    "url": "https://anclick.veanx.tech/static/js/main.ab4620bf.chunk.js"
+  },
+  {
+    "revision": "67304111f09ffe920099",
+    "url": "https://anclick.veanx.tech/static/js/runtime-main.457908bf.js"
+  },
+  {
+    "revision": "1f7beca5bf4cda1803cd59cd881c372d",
+    "url": "https://anclick.veanx.tech/static/media/100_1.1f7beca5.jpg"
+  },
+  {
+    "revision": "82e7c41e073394f808fd382c97e71884",
+    "url": "https://anclick.veanx.tech/static/media/100_2.82e7c41e.jpg"
+  },
+  {
+    "revision": "6e25d86d33b97ab231889fd4a38530b4",
+    "url": "https://anclick.veanx.tech/static/media/100_3.6e25d86d.jpg"
+  },
+  {
+    "revision": "978e51b554db00a67e2796661b3526fe",
+    "url": "https://anclick.veanx.tech/static/media/100_4.978e51b5.jpg"
+  },
+  {
+    "revision": "fd53372585bf0ef377e97ee9dd7ea925",
+    "url": "https://anclick.veanx.tech/static/media/100_5.fd533725.jpg"
+  },
+  {
+    "revision": "c4f9752ad87fca526faf0fc1f2a408c9",
+    "url": "https://anclick.veanx.tech/static/media/anclick.c4f9752a.jpeg"
+  },
+  {
+    "revision": "c54eef50718560d1268aa8e92025972d",
+    "url": "https://anclick.veanx.tech/static/media/background_1920-2.c54eef50.jpg"
+  },
+  {
+    "revision": "dc8b1732c919ca17845aab44dc3afb27",
+    "url": "https://anclick.veanx.tech/static/media/cap1.dc8b1732.png"
+  },
+  {
+    "revision": "258d3697e470dbe336de9c4740cd4567",
+    "url": "https://anclick.veanx.tech/static/media/empty_logo.258d3697.jpg"
+  },
+  {
+    "revision": "d888b3a7051230608ec0c1dc248bbeb4",
+    "url": "https://anclick.veanx.tech/static/media/login1.d888b3a7.jpg"
+  },
+  {
+    "revision": "4a30958332ac703c28a40e35eb93dcf0",
+    "url": "https://anclick.veanx.tech/static/media/login2.4a309583.jpg"
+  },
+  {
+    "revision": "624f11a53ecad7f6c2ca08adbbd472af",
+    "url": "https://anclick.veanx.tech/static/media/login5.624f11a5.jpg"
+  },
+  {
+    "revision": "80d4a4e5af9b6cf3882e4a34c5497542",
+    "url": "https://anclick.veanx.tech/static/media/sidebar-4.80d4a4e5.jpg"
+  }
+]);
