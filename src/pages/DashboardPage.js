1bda0047551d17174eb152798a94580eb426f43c
Alvaro Guedez <alvaro.develoop@gmail.com>
2020-12-28 18:29:17 -0400

    commit

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import Page from 'components/Page';
 import EnterpriseDashboardComponent from 'components/dashboards/EnterpriseDashboardComponent'
 import {connect} from 'react-redux'
+import {Row,Col,Button} from 'react-bootstrap'
 
 const DashboardPage = props => {
 
@@ -10,17 +11,37 @@ const DashboardPage = props => {
     props.history.replace(route)
   }
 
-    return (
-      <Page
-        className="DashboardPage"
-        title="Dashboard"
-        breadcrumbs={[{ name: 'Dashboard', active: true }]}
-      >
-        {props.user.id_rol >= 2 ? (
-          <EnterpriseDashboardComponent {...props} />
-        ) : ''}
-      </Page>
-    );
+  return (
+    <Page
+      className="DashboardPage"
+      title="Dashboard"
+      breadcrumbs={[{ name: 'Dashboard', active: true }]}
+    >
+      {props.id_enterprise ? (
+        <React.Fragment>
+          {props.id_branch_office ? (
+            <React.Fragment>
+              {props.user.id_rol >= 2 ? (
+                <EnterpriseDashboardComponent {...props} />
+              ) : ''}
+            </React.Fragment>
+          ) : (
+            <Row className="align-items-center">
+              <Col>
+                <p className="alert alert-info text-center">Escoja una sucursal para trabajar</p>
+              </Col>
+            </Row>
+          )}
+        </React.Fragment>
+      ) : (
+        <Row className="justify-content-center">
+          <Col sm={8} md={6} lg={6} xl={6}>
+            <Button type="button" size="sm" onClick={() => redirectRoute("/enterprise/form")} block={true}>Crear Empresa</Button>
+          </Col>
+        </Row>
+      )}
+    </Page>
+  );
 
 }
 
@@ -34,6 +55,7 @@ function mapStateToProps(state){
     user : state.auth.user,
     configGeneral : state.configs.config,
     id_branch_office : state.enterpriseSucursal.id_branch_office,
+    id_enterprise : state.enterpriseSucursal.id_enterprise,
   }
 }
 
